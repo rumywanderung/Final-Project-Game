dfc0a33544e16705d04383c9314166bc2b2d4bc9
Rumy <rbourkov@gmail.com>
2019-11-15 15:30:43 +0100

    Initial commit

---
+++ 09-10-19 - Copy/Assets/Scripts/MoveToPoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEditor;
+
+public class MoveToPoint : MonoBehaviour
+{
+
+    public GameObject Player;
+    public Cinemachine.CinemachineVirtualCamera Cam;
+    public GameObject Point;
+    public GameObject dollycart;
+    public GameObject dollytrack;
+    public Camera maincam;
+    public Cinemachine.CinemachineVirtualCamera MainCam;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger");
+
+        if (Player.tag == "player")
+        {
+            /*this.Cam.Follow = Point.transform;
+            this.Cam.LookAt = Point.transform;*/
+
+            //float length = dollytrack.GetComponent<CinemachinePath>().PathLength;
+            //length = 1;
+            Cam = Instantiate(Resources.Load("CM vcam1", typeof(Cinemachine.CinemachineVirtualCamera))) as Cinemachine.CinemachineVirtualCamera;
+          
+            dollycart = Instantiate(Resources.Load("DollyCart1", typeof(GameObject))) as GameObject;
+            Cam.m_Follow = dollycart.transform;
+            Cam.m_LookAt = Point.transform;
+
+            dollytrack = Instantiate(Resources.Load("DollyTrack1", typeof(GameObject))) as GameObject;
+            //dollytrack.GetComponent<CinemachineSmoothPath>().m_Waypoints[1].position.x = 5.529957F;
+            //dollytrack.GetComponent<CinemachineSmoothPath>().m_Waypoints[1].position.y = 27.78951F;
+            //dollytrack.GetComponent<CinemachineSmoothPath>().m_Waypoints[1].position.z = -3.959243F;
+            dollycart.GetComponent<CinemachineDollyCart>().m_Path = dollytrack.GetComponent<CinemachineSmoothPath>();
+            Debug.Log(Cam.Follow);
+        }
+    }
+}
---
+++ 09-10-19 - Copy/Assets/Scripts/Player_Grabbing.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Grabbing : MonoBehaviour
+{
+    public bool grabbed = false;
+    public bool closeEnough = false;
+    private Transform grabbedObject = null;
+    public Transform playerCam;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "objectToGrab")
+        {
+            closeEnough = true;
+            grabbedObject = collision.collider.GetComponent<Transform>();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "objectToGrab")
+        {
+            closeEnough = false;
+        }
+    }
+
+    void Update()
+    {
+        if (grabbedObject != null)
+        {
+            float dist = Vector3.Distance(grabbedObject.gameObject.transform.position, this.transform.position);
+
+            if (dist <= 2.5F)
+            {
+                if (Input.GetKeyUp("g"))
+                {
+                    if (closeEnough == true)
+                    {
+                        Debug.Log("grabbed");
+                        grabbed = true;
+                        GetComponent<Rigidbody>().isKinematic = true;
+                        grabbedObject.transform.parent = playerCam;
+                    }
+                }
+            }
+
+            else
+            {
+                grabbed = false;
+            }
+        }
+    }
+}
