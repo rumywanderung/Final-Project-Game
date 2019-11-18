e16a437fd07a67f53181c54c730bec29ed15464f
Rumy <rbourkov@gmail.com>
2019-11-18 16:45:38 +0100

    working on custom methods in VIDE

---
+++ 09-10-19/Assets/Scripts/NPC_GuestA_1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_GuestA_1 : MonoBehaviour
+{
+    public GameManager manager;
+    GameObject grabableobject;
+
+    public void Start()
+    {
+        manager = FindObjectOfType<GameManager>();
+    }
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+           
+            player = collision.gameObject;
+
+            if (player.GetComponent<Player_Grabbing>().grabbedObject.name == "wine")
+            {
+                grabableobject = player.GetComponent<Player_Grabbing>().grabbedObject.gameObject;
+                DestroyObject(grabableobject);
+                
+            }
+
+        }
+    }*/
+
+    public void DestroyObject()
+    {
+        grabableobject = manager.Player.GetComponent<Player_Grabbing>().grabbedObject.gameObject;
+        Destroy(grabableobject);
+    }
+}
