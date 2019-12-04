a41c2084d673b0dcc54661cbfeffc63a5e470a6c
Rumy <rbourkov@gmail.com>
2019-12-04 12:07:04 +0100

    linked camera event to dialogue options

---
+++ Masquerade 2/Assets/Scripts/NPC_Events.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEditor;
+
+public class NPC_Events : MonoBehaviour
+{
+    //destroyObject
+    public GameManager manager;
+    GameObject grabableobject;
+    //cameraEvent
+    public Cinemachine.CinemachineVirtualCamera Cam;
+    public GameObject LookAts;
+    public GameObject Cue;
+    public GameObject dollycart;
+    public GameObject dollytrack;
+
+    public void Start()
+    {
+        manager = FindObjectOfType<GameManager>();
+    }
+
+    public void DestroyObject()
+    {
+        grabableobject = manager.Player.GetComponent<Player_Grabbing>().inHand.gameObject;
+        Destroy(grabableobject);
+    }
+
+    public void CameraEvent()
+    {
+        dollycart = Instantiate(Resources.Load("DollyCart1", typeof(GameObject))) as GameObject;
+        Cam = FindObjectOfType<Cinemachine.CinemachineVirtualCamera>();
+        Cam.m_Follow = dollycart.transform;
+        Cam.m_LookAt = LookAts.transform;
+
+        dollycart.GetComponent<CinemachineDollyCart>().m_Path = dollytrack.GetComponent<CinemachineSmoothPath>();
+        Debug.Log(Cam.Follow);
+    }
+}
