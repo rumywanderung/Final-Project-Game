084d627e54cc543110dcc5de1d7b505a7f10304a
Rumy <rbourkov@gmail.com>
2019-12-09 16:27:46 +0100

    ((safety))

--- Masquerade 2/Assets/Scripts/Introduction.cs
+++ Masquerade 2/Assets/Scripts/Introduction.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Introduction : MonoBehaviour
 {
-
+    public GameObject Player;
+    public GameObject intro00;
     public GameObject intro01;
     private bool inTrigger = false;
     private bool deletion0 = false;
+    private bool deletion1 = false;
     public GameObject info;
     public GameObject particles;
-    private AudioSource sfx;
+    public AudioSource sfx;
+    public AudioClip sfxClip;
 
     private void Awake()
     {
@@ -26,27 +29,42 @@ public class Introduction : MonoBehaviour
             inTrigger = true;
         }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        DestroyObjects(sfx.gameObject);
+    }
 
     private void Update()
     {
         if (this.name == "I: 00" && inTrigger == true)
         {
             GameObject intro = Instantiate(Resources.Load("INTRO 00") as GameObject);
-            intro01 = intro;
+            intro00 = intro;
             inTrigger = false;
             deletion0 = true;
         }
         else if (this.name == "I: 01" && inTrigger == true)
         {
-
+            GameObject intro = Instantiate(Resources.Load("INTRO 00") as GameObject);
+            intro00 = intro;
+            inTrigger = false;
+            deletion1 = true;
         }
         //when to delete
         if (deletion0 == true)
         {
             if (Input.GetKeyUp("e"))
             {
-                DestroyObjects(intro01);
+                DestroyObjects(intro00);
                 deletion0 = false;
+            }
+        }
+        else if (deletion1 == true)
+        {
+            if (Input.GetKeyUp("e"))
+            {
+                DestroyObjects(intro00);
+                deletion1 = false;
             }
         }
     }
