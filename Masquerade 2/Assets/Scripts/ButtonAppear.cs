24ba4f61b8581fde40c0430a2f5586dc72cf0465
Rumy <rbourkov@gmail.com>
2019-12-11 12:37:45 +0100

    Start Page

---
+++ Masquerade 2/Assets/Scripts/ButtonAppear.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ButtonAppear : MonoBehaviour
+{
+    private Button buttonStart;
+    private Button buttonExit;
+    private GameObject CanvasButtons;
+
+    public ButtonMenu Canvas;
+    public float i = 0;
+
+    void Start()
+    {
+        CanvasButtons = (GameObject)Resources.Load("CanvasButtons");
+    }
+    
+    void Update()
+    {
+        if (i < 8)
+        {
+            i += Time.deltaTime;
+        }
+        else if (i >= 8)
+        {
+            Instantiate(CanvasButtons);
+            if (CanvasButtons.GetComponentInChildren<Button>().name == "ButtonStart")
+            {
+                buttonStart = CanvasButtons.GetComponentInChildren<Button>();
+            }
+            else if (CanvasButtons.GetComponentInChildren<Button>().name == "ButtonExit")
+            {
+                buttonExit = CanvasButtons.GetComponentInChildren<Button>();
+            }
+            
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Masquerade 2/Assets/Scripts/ButtonMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MAIN");
+    }
+
+    public void EndGame()
+    {
+          Application.Quit();
+    }
+
+    /*public void DisplaySliderValue(float value)
+    {
+        Debug.Log("Slider value: " + value.ToString());
+    }*/
+}
